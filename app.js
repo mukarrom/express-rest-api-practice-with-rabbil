25a8e6400840d820ceca32758d846c640c862417
mukarrom <mukarrom.hosain.1@gmail.com>
2023-09-23 18:44:12 +0600

    Undefined route handeled with 404 status code

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ const app = new express();
 // we can manage api version from here
 app.use('/api/v1', router);
 
+// Undefined Route
+app.use('*', (req, res) => {
+	res.status(404).json({ status: 'Failed', data: 'Not found' });
+});
+
 module.exports = app;
