f8edf3b8e040a3220b50c36cc1bf8247ee8f1d9b
mukarrom <mukarrom.hosain.1@gmail.com>
2023-09-23 19:02:46 +0600

    Security middlewares implemented

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ const router = require('./src/routes/api');
 // instance of express
 const app = new express();
 
+// Security Middleware import
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+
+// Security middleware implement
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
+// Request Rate Limiting
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+});
+
+app.use(limiter);
+
 // we can manage api version from here
 app.use('/api/v1', router);
 
